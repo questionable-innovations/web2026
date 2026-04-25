e41609f81682ab2c07df49d772d3504fdac4fb48
Taine Reader <taine.reader@gmail.com>
2026-04-25 18:57:46 +1200

    Revert "feat: vibe slop"
    This reverts commit 22307ff245655ec9a870a1627db5349aa710c79a.

--- contracts/script/utils/Script.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
-
-import {Vm} from "../../test/utils/Test.sol";
-
-abstract contract Script {
-    Vm internal constant vm =
-        Vm(address(uint160(uint256(keccak256("hevm cheat code")))));
-}
